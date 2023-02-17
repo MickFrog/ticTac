fbdd1af938c26a2644e9b665f6cf2e335a503a84
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-17 12:18:49 +0300

    add game board on choosing opponent

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 //Acquire elements
-const gameCards = [...document.querySelectorAll('.gameCard')];
 const humanBtn = document.getElementById('humanBtn');
 const robotBtn = document.getElementById('botBtn');
 
 //Event listeners
 function initialiseGameCards() {
-    for (let i = 0; i < gameCards.length; i++) {
-        gameCards[i].addEventListener('click', (event) => {
-            event.target.classList.add('X-card');
-        });
+    //clear gameBoard
+    const gameBoard = document.querySelector('.gameBoard');
+    
+    while(gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
     }
+
+    for (let i = 0; i < 9; i++) {
+        gameBoard.appendChild(createCard());
+    }
+}
+
+function createCard() {
+    const card = document.createElement('div');
+    card.classList.add('gameCard');
+
+    card.addEventListener('click', ()=> {
+        card.classList.add('X-card');
+    });
+
+    return card;
 }
 
 humanBtn.addEventListener('click', ()=> {
     humanBtn.className = 'clicked';
     robotBtn.className = '';
 
+    initialiseGameCards();
 });
 
 robotBtn.addEventListener('click', ()=> {
     robotBtn.className = 'clicked';
     humanBtn.className = '';
 
+    initialiseGameCards();
 });
-
-//Driver code
-initialiseGameCards();
