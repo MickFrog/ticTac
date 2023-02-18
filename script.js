965f04e3af9df48bd3dd8924b68e651762432824
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-18 10:49:46 +0300

    Add player switching ability

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Global variables
+let currPlayer = null;
+
 //Acquire elements
 const humanBtn = document.getElementById('humanBtn');
 const robotBtn = document.getElementById('botBtn');
@@ -36,9 +39,32 @@ function createCard() {
     const card = document.createElement('div');
     card.classList.add('gameCard');
 
-    card.addEventListener('click', ()=> {
-        card.classList.add('X-card');
+    card.addEventListener('click', ()=> { //show current player's name on card
+        card.classList.add(`${currPlayer.name}-card`);
+        switchPlayer();
     });
 
     return card;
 }
+
+const Player = (name) => {
+    this.name = name;
+
+    this.checkWinCondition = () => {
+
+    };
+
+    return {name, checkWinCondition};
+};
+
+function switchPlayer() { //Switch Players by changing name
+    if (currPlayer.name == 'X') {
+        currPlayer.name = 'O';
+        return;
+    } 
+    currPlayer.name = 'X';
+}
+
+//Driver code
+let newPlayer = Player('X');
+currPlayer = newPlayer;
